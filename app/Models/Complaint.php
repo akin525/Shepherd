ac581662f50eb87393e80292cd6670324c58e921
HP <samson.akinlabi@cashonrails.com>
2026-02-05 06:50:31 +0100

    new

--- app/Models/Complaint.php
+++ app/Models/Complaint.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Complaint extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'complaint_from',
