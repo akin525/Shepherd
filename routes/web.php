8fc06f6d549bff033e74d0a4ae8afbc22d8c080f
HP <samson.akinlabi@cashonrails.com>
2026-03-24 23:51:56 +0100

    newss

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    $targetFolder = storage_path('app/public');
+    $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage';
+
+    try {
+        symlink($targetFolder, $linkFolder);
+        return 'Success! The storage link has been created for cPanel.';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
+});
