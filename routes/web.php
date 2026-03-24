e28c3b32adac6c472ff479bf225edf776ada1ee8
HP <samson.akinlabi@cashonrails.com>
2026-03-24 23:52:54 +0100

    newss

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/', function () {
     $targetFolder = storage_path('app/public');
